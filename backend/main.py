27980896b64fb516c2e0dd0123416f98dfbeba8e
SM Sadat <smsadat788@gmail.com>
2026-01-13 19:09:51 +0600

    refactor: New backend replacement(2)

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,30 @@
+import os
+from dotenv import load_dotenv
 import re
 
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Union
 
+load_dotenv()
+FRONTEND_URL1 = os.getenv('FRONTEND_URL1')
+FRONTEND_URL2 = os.getenv('FRONTEND_URL2')
+
 app = FastAPI()
+
+origins = [
+    FRONTEND_URL1,
+    FRONTEND_URL2
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=str(origins),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 def validate_user_input(username: str, email: str, password: str):
     # Username: only letters, numbers, underscores
