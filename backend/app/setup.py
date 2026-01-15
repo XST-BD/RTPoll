38e9ca75ddd0f92a6ec87f8ea9fe818d40c8718a
SM Sadat <smsadat788@gmail.com>
2026-01-15 11:46:50 +0600

    refactor: Centralize all dotenv to one file

--- backend/app/setup.py
+++ backend/app/setup.py
@@ -1,14 +1,25 @@
 import os
 
+from dotenv import load_dotenv
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-def cors_permit():
+load_dotenv()
 
-    FRONTEND_URL1 = os.getenv('FRONTEND_URL1')
-    FRONTEND_URL2 = os.getenv('FRONTEND_URL2')
+BACKEND_URL1 = os.getenv('BACKEND_URL1')
+FRONTEND_URL1 = os.getenv('FRONTEND_URL1')
+FRONTEND_URL2 = os.getenv('FRONTEND_URL2')
+DATABASE_URL = os.getenv('DATABASE_URL')
+SENDER_MAIL = os.getenv('MAIL_USERNAME', 'MAIL_USERNAME')
+APP_PASSWORD = os.getenv('MAIL_PASSWORD')
+SMTP_SERVER = os.getenv('MAIL_SERVER')
+SMTP_PORT_TLS = os.getenv('MAIL_PORT_TLS', 587)
+
+
+def cors_permit():
 
     origins = [
         FRONTEND_URL1,
