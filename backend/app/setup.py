4bc1217f6f288ff7211d77ee2bb5eb89692303b9
SM Sadat <smsadat788@gmail.com>
2026-01-19 19:16:50 +0600

    fix: Edit starletter middlware cfg

--- backend/app/setup.py
+++ backend/app/setup.py
@@ -41,8 +41,9 @@ def cors_permit():
 
     app.add_middleware(
         SessionMiddleware, 
+        session_cookie='session',
         secret_key=str(SECRET_KEY),
         max_age=60*60*24*7,            # 1 week
-        https_only=False,              # True in production
-        same_site="lax",
+        https_only=False,  # must be False on localhost HTTP
+        same_site="none",  # allows cross-site JS requests to send cookie
     )
