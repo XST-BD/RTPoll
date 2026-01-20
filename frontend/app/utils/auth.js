cd79a73840cb9455228e01d6a92641f03f61ef5d
Atia-Farha <119663174+Atia-Farha@users.noreply.github.com>
2026-01-20 21:23:06 +0600

    fix: cache code

--- frontend/app/utils/auth.js
+++ frontend/app/utils/auth.js
@@ -2,11 +2,9 @@ export async function isLoggedIn() {
     try {
         const res = await $fetch('http://127.0.0.1:8000/api/v0/auth/check', {
             method: "GET",
-            credentials: 'include',
-            cache: 'no-store'
+            credentials: 'include'
         })
 
-        // return true
         return res.authenticated === true
     } catch {
         return false
