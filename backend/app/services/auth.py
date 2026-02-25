bb03fb63a086a24a0956c3cda245bc28d049be9f
SM Sadat <smsadat788@gmail.com>
2026-02-25 19:39:10 +0600

    hudai(4)

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -13,7 +13,7 @@ from app.deps import hash_password, verify_password, get_db
 from app.setup.vars import SECRET_KEY
 
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 15
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
