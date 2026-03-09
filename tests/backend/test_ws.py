69f0609982e32b6f33c162f5994a8d54bee27e27
SM Sadat <smsadat788@gmail.com>
2026-03-09 13:43:09 +0600

    chore: Update docker configs

--- tests/backend/test_ws.py
+++ tests/backend/test_ws.py
@@ -3,17 +3,35 @@ import json
 import websockets
 
 async def test_ws_vote_view():
-    uri = "ws://127.0.0.1:8000/ws/vote/1"
+    uri = "ws://127.0.0.1:8000/ws/vote/3"
 
     async with websockets.connect(uri) as websocket:
         
         async def send_message():
+            msg = {"type": "get_vote"}
+            await websocket.send(json.dumps(msg))
+            await asyncio.sleep(1)
+
+        async def read_message():
             while True: 
-                msg = {
-                    "type": "send_vote",
-                    "option_id": "Me again",
-                }
-                await websocket.send(json.dumps(msg))
+                response = await websocket.recv()
+                try: 
+                    data = json.loads(response)
+                    print(data)
+                except json.JSONDecodeError:
+                    print(response)
+
+        await asyncio.gather(send_message(), read_message())
+
+async def test_ws_vote_sent():
+    uri = "ws://127.0.0.1:8000/ws/vote/3"
+
+    async with websockets.connect(uri) as websocket:
+        
+        async def send_message():
+            msg = {"type": "send_vote", "option_id": 1}
+            await websocket.send(json.dumps(msg))
+    
 
         async def read_message():
             while True: 
@@ -41,7 +59,7 @@ async def test_ws_poll_view():
 
         async def read_message(): 
             while True: 
-                response  = await websocket.recv()
+                response  = await websocket.recv() 
                 try: 
                     data = json.loads(response)
                     print(data)
@@ -51,5 +69,5 @@ async def test_ws_poll_view():
         await asyncio.gather(send_message(), read_message())
 
 
-asyncio.run(test_ws_poll_view())     
+asyncio.run(test_ws_vote_sent())     
         
