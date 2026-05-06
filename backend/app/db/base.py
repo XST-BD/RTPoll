fce9d0b2e9a2f68948eb55b07f2d6dd168ae8e04
smsadat1 <smsadat788@gmail.com>
2026-05-07 00:59:13 +0600

    chore: Move db to Postgres

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL2', 'DATABASE_URL2')
 dbengine =  create_engine(
     url=DATABASE_URL,
     # connect_args={"check_same_thread": False}   # sqlite only
