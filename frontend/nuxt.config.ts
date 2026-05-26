49504fa110885d58f567e9331a927e05cd0db724
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2026-05-26 22:56:04 +0600

    chore: add pnpm workspace configuration to allow specific builds

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,26 +1,47 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
-	devtools: { enabled: false },
-	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-	css: ['@/assets/css/global.css'],
+
+	devtools: {
+		enabled: false
+	},
+
+	debug: false,
+
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@pinia/nuxt'
+	],
+
+	css: [
+		'@/assets/css/global.css'
+	],
+
 	devServer: {
 		host: '0.0.0.0'
 	},
+
 	runtimeConfig: {
 		public: {
-			apiBase: import.meta.env.API_URL,
-			wsBase: import.meta.env.WS_URL
+			apiBase: process.env.API_URL,
+			wsBase: process.env.WS_URL
 		}
 	},
+
 	routeRules: {
-		'/verify-mail': { ssr: false }
+		'/verify-mail': { ssr: false },
+		'/dashboard/**': { ssr: false }
 	},
+
 	vite: {
 		optimizeDeps: {
-			include: ['vue3-apexcharts', 'apexcharts']
+			include: [
+				'apexcharts',
+				'vue3-apexcharts'
+			]
 		}
 	},
+
 	nitro: {
 		preset: 'cloudflare-pages'
-	},
+	}
 })
