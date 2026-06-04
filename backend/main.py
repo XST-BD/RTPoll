35c3ef07d86e1d9ca0d9b1850ee728eb3f0ac35f
smsadat1 <smsadat788@gmail.com>
2026-06-05 02:23:54 +0600

    Update main file and lua and docker compose config

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from app.api import auth, user, verify, poll
+from app.api import auth, user, verify, poll, voter, token
 from app.api.ws import polling
 from app.db.base import Base, dbengine
 from app.setup.vars import app, router
@@ -10,7 +10,7 @@ from app.db.model.poll import PollModel
 
 cors_permit()
 
-# Base.metadata.create_all(bind=dbengine)
+Base.metadata.create_all(bind=dbengine)
 
 @router.get('/')
 async def response_root():
@@ -20,6 +20,8 @@ router.include_router(auth.router, prefix='/api/v0/auth', tags=['Authentication 
 router.include_router(verify.router, prefix='/api/v0/auth/email', tags=['Mail verification endpoints'])
 router.include_router(user.router, prefix='/api/v0/user', tags=['User state management endpoints'])
 router.include_router(poll.router, prefix='/api/v0/poll', tags=['Poll state management endpoints'])
+router.include_router(token.router, prefix='/api/v0/poll', tags=['Poll token management endpoints'])
+router.include_router(voter.router, prefix='/api/v0/voter', tags=['Voter endpoints'])
 
 router.include_router(polling.router, prefix='/api/v0/ws', tags=['Websocket endpoints for poll creator'])
 
